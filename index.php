1e92f6f17124b90dad9d6adc83d17a92fd6d596e
vavarrm <vavarrm@gmail.com>
2018-07-16 13:10:44 +0700

    !~E_WARNING

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ switch (ENVIRONMENT)
 	case 'development':
 		error_reporting(-1);
 		ini_set('display_errors', 1);
-		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED&~E_PARSE);
+		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED& ~E_WARNING );
 	break;
 
 	case 'testing':
